f55dd07b7b4b99857bcfac2c498948fc5a394c91
Hanna Borodina <venera131916@gmail.com>
2023-05-27 02:22:46 +0300

    add battlefield app

---
+++ appManager/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include "menuApps.h"
+using namespace std;
+
+void main()
+{
+menuApps();
+}
---
+++ appManager/menuApps.h
@@ -0,0 +1,5 @@
+#ifndef MENU_APPS_H
+#define MENU_APPS_H
+void menuApps();
+
+#endif 
